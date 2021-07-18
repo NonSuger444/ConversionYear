5f9478a44e5cc901dd92ea4bd5cd2f181020303b
NonSuger444 <crowrabbit1212@gmail.com>
2021-07-18 14:57:44 +0900

    年号指定箇所変更
    テキストボックスを以下のように変更
    年号指定：コンボボックス
    年指定：テキストボックス

--- main.py
+++ main.py
@@ -24,16 +24,48 @@ frame.pack()
 # ###############################################
 # ウィジェット生成
 # > frameを親要素とする
+# > justify : 文字揃え
+# > state : 状態
+# >> readonly : 読み込み専用 -> 書き込み不可
 # ###############################################
-# ラベル生成
-label = ttk.Label(frame, text='年を入力')
+# コンボボックス生成
+cbox = ttk.Combobox(frame,
+                    width=8,
+                    justify='center',
+                    state='readonly',
+                    values=['西暦', '令和', '平成', '昭和'])
 # 入力ボックス生成
-ibox = ttk.Entry(frame, width=10, justify='right')
+ibox = ttk.Entry(frame,
+                 width=10,
+                 justify='right')
+# ラベル生成 - 西暦
+era1 = ttk.Label(frame,
+                 text='西暦 : ')
+nen1 = ttk.Label(frame,
+                 text=2021)
+# ラベル生成 - 令和
+era2 = ttk.Label(frame,
+                 text='令和 : ')
+nen2 = ttk.Label(frame,
+                 text=3)
+# ラベル生成 - 平成
+era3 = ttk.Label(frame,
+                 text='平成元年から : ')
+nen3 = ttk.Label(frame,
+                 text=33)
+# ラベル生成 - 昭和
+era4 = ttk.Label(frame,
+                 text='昭和元年から : ')
+nen4 = ttk.Label(frame,
+                 text=96)
 # ボタン生成
-btn1 = ttk.Button(frame, text='換算')
+btn1 = ttk.Button(frame,
+                  text='換算')
 # ボタン生成
 # => root.destroy : ウィンドウ終了命令を設定
-btn2 = ttk.Button(frame, text='終了', command=root.destroy)
+btn2 = ttk.Button(frame,
+                  text='終了',
+                  command=root.destroy)
 
 # ###############################################
 # ウィジェット配置
@@ -41,10 +73,20 @@ btn2 = ttk.Button(frame, text='終了', command=root.destroy)
 # > column : 列
 # > padx : 左右の空き具合
 # > pady : 上下の空き具合
+# > sticky : ラベルの文字揃え
+# >> e : 右揃え
 # ###############################################
 # ラベル･入力ボックス･ボタンをgridで画面上に配置
-label.grid(row=0, column=0, padx=5, pady=10)
+cbox.grid(row=0, column=0, padx=5, pady=10)
 ibox.grid(row=0, column=1, padx=2, pady=10)
+era1.grid(row=1, column=0, padx=5, pady=3, sticky='e')
+nen1.grid(row=1, column=1, padx=2, pady=3, sticky='e')
+era2.grid(row=2, column=0, padx=5, pady=3, sticky='e')
+nen2.grid(row=2, column=1, padx=2, pady=3, sticky='e')
+era3.grid(row=3, column=0, padx=5, pady=3, sticky='e')
+nen3.grid(row=3, column=1, padx=2, pady=3, sticky='e')
+era4.grid(row=4, column=0, padx=5, pady=3, sticky='e')
+nen4.grid(row=4, column=1, padx=2, pady=3, sticky='e')
 btn1.grid(row=5, column=0, padx=5, pady=3)
 btn2.grid(row=5, column=1, padx=2, pady=3)
 
